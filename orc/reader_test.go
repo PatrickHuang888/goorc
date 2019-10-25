e26a405180a4fe8cd34eee5ad102eae2522c0712
hxm <huangxiaoman@gmail.com>
2019-10-25 14:53:54 +0800

    refactoring reader int run length v2 direct with readbits

---
+++ orc/reader_test.go
@@ -0,0 +1,36 @@
+package orc
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNoCompression(t *testing.T)  {
+	opts := DefaultReaderOptions()
+	reader, err := NewReader("../testing/basicLongNoCompression.orc", opts)
+	if err != nil {
+		t.Errorf("create reader error: %+v", err)
+	}
+
+	schema := reader.GetSchema()
+	stripes, err := reader.Stripes()
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+
+	for _, stripe := range stripes {
+		batch, err := schema.CreateReaderBatch(opts)
+		if err != nil {
+			t.Errorf("create row batch error %+v", err)
+			os.Exit(1)
+		}
+
+		for next := true; next; {
+			next, err = stripe.NextBatch(batch)
+			if err != nil {
+				t.Fatalf("%+v", err)
+				break
+			}
+		}
+	}
+}
